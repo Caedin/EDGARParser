396351395462099cbba43f43b1cb83c558ee972f
Keith Dyer <kilaxen@gmail.com>
2015-05-09 19:40:03 -0500

    Updated main.py to have changes associated with the refactoring.

--- Code and Utility Files/EclipseProject/src/EdgarParser/Main.py
+++ Code and Utility Files/EclipseProject/src/EdgarParser/Main.py
@@ -7,33 +7,29 @@ import glob
 import os
 from Filing import Filing
 
-def Find_Most_Likely_Statement_In_Folder(folder):
-    #Takes the path to a folder, returns the path to the largest file in that folder of type .htm.
-    files = glob.glob(folder + "\\*")
-    html_files = [item for item in files if 'htm' in item.split('.')[-1] or 'txt' in item.split('.')[-1]]
-    if len(html_files) == 0:
-        print 'Error, no files found in the folder.', folder
+# Accepts path to a folder, and returns the most likely file with the cash flow table. (10K)
+def Find10KInFolder(folder):
+    filesInFolder = glob.glob(folder + "\\*")
+    htmlFiles = [item for item in filesInFolder if 'htm' in item.split('.')[-1] or 'txt' in item.split('.')[-1]]
+    if len(htmlFiles) == 0:
+        print 'Error, no HTML Files found in the folder.', folder
         return 
     else:
-        html_files.sort(key=lambda x: os.path.getsize(x))
-        html_files.reverse()
-        return html_files[0]
+        htmlFiles.sort(key=lambda x: os.path.getsize(x))
+        htmlFiles.reverse()
+        return htmlFiles[0]
 
-def Find_All_10K(company_check_file):
-    #Loop through folder and find all 10k statements
-    #Avoid companies not listed in the check_file
-    #Returns a list of paths to files which should contain the 10k form.
-    
-    # Key is company ID, value is list of reporting years
-    companies_to_extract = {}
-    with open(company_check_file, 'rb') as input_stream:
+# Accepts a file with a list of company and year report pairs. Finds all available reports from the list, and returns a list of file paths to their respective 10K documents.
+def FindEligibleReports(eligibleCompanyReports):
+    eligibleCompanyReportDictionary = {}
+    with open(eligibleCompanyReports, 'rb') as input_stream:
         for line in input_stream:
             line = line.strip()
             line = line.split(',')
-            if line[0] not in companies_to_extract:
-                companies_to_extract[line[0]] = [line[1]]
+            if line[0] not in eligibleCompanyReportDictionary:
+                eligibleCompanyReportDictionary[line[0]] = [line[1]]
             else:
-                companies_to_extract[line[0]].append(line[1])
+                eligibleCompanyReportDictionary[line[0]].append(line[1])
     
     
     initial_paths = glob.glob('C:\\Users\\Keith\\Desktop\\Workspace\\Work - Summer 2015\\EDGARParser\\10KMASTER\\Y2009-Y2010\\Filings\\*\\*')
@@ -43,26 +39,23 @@ def Find_All_10K(company_check_file):
         cik= path.split('\\')[-2]
         
         try:
-            if year in companies_to_extract[cik]:
+            if year in eligibleCompanyReportDictionary[cik]:
                 pruned_paths.append(path)
         except KeyError:
             #Company not found in check file, skipping.
             pass
         
-    list_of_annual_reports = []
+    AnnualReports = []
     for k in pruned_paths:
-        list_of_annual_reports.append(Find_Most_Likely_Statement_In_Folder(k))
-    return list_of_annual_reports
-
-            
+        AnnualReports.append(Find10KInFolder(k))
+    return AnnualReports
 
 if __name__ == '__main__':
-    tenK_reports = Find_All_10K("C:\\Users\\Keith\\Desktop\\Workspace\\Work - Summer 2015\\EDGARParser\\Code and Utility Files\\cikyearcomp0910.csv")
-    list_of_filings = [0.0] * len(tenK_reports)
-    for i, k in enumerate(tenK_reports):
-        print i+1, ' of ', len(tenK_reports)
-        list_of_filings[i] = Filing(k)
-        list_of_filings[i].save_CFs_to_file();
+    AnnualReports = FindEligibleReports("C:\\Users\\Keith\\Desktop\\Workspace\\Work - Summer 2015\\EDGARParser\\Code and Utility Files\\cikyearcomp0910.csv")
+    for i, k in enumerate(AnnualReports):
+        print i+1, ' of ', len(AnnualReports)
+        AnnualReports[i] = Filing(k)
+        AnnualReports[i].saveCashFlowsToFile()
     
 
     
